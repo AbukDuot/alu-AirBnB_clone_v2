703ed71e4a63aa2c05636e3aa60d56070f24bf88
Humphrey <h.nyahoja@alustudent.com>
2024-01-30 15:17:40 -0800

    MySQL setup development - 5

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
---the existance of the database is checked before creating it
+-- the existance of the database is checked before creating it
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---the existance of the user is checked before creating it
+-- the existance of the user is checked before creating it
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
---privileges are granted to the user
+-- privileges are granted to the user
 GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
