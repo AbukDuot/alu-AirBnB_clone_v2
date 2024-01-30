cd37d396e300a0b85bb3432d06f75c6e8eb19146
Humphrey <h.nyahoja@alustudent.com>
2024-01-30 13:31:50 -0800

    MySQL setup development correction 4

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,7 @@
 --the existance of the database is checked before creating it
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `hbnb_dev_db`;
-
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 --the existance of the user is checked before creating it
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 --privileges are granted to the user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
-
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
