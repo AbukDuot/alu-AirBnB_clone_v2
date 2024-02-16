6b6f858e2f68ceece5f57af0a3ffb94eb8bcaf56
AbukDuot <a.duotalustudent.com>
2024-02-16 07:58:54 -0800

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-#this script sets up web servers deployment of web_static
-ssh -i /root/.ssh/ssh_rsa ubuntu@34.229.12.144 << EOF
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "<html> <b>HUMPHREY NYAHOJA.</b> </html>" | sudo tee /data/web_static/releases/test/index.html
-sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-sudo service nginx restart
-EOF
+# Script that configures Nginx server with some folders and files
+apt-get -y update
+apt-get -y install nginx
+service nginx start
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
+exit 0
