f25e505d1031952a82bca032496d312dc2591493
Humphrey <h.nyahoja@alustudent.com>
2024-02-16 03:54:22 +0000

    add documentation to 1-pack_web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""creates a tgz archive for webstaic files """
 from fabric.api import local, env
 from datetime import datetime
 
 def do_pack():
+    """generates the tgz archive"""
     local("mkdir -p versions")
     time_now = datetime.now().strftime("%Y%m%d%H%M%S")
     path = "versions/web_static_{}.tgz".format(time_now)
